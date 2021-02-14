c95d764df818cffcfb545b498ca14640536f5db0
NotLiu <aliu@lediur.com>
2021-02-13 21:06:43 -0600

    implemented p5 coordinate plane

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,22 +11,47 @@ class App extends React.Component{
         this.myRef = React.createRef();
 
         this.state = {
-            count:0
+            count:0,
+            gridSize:[-10,10,-10,10,50,50] //minx, maxx, miny, maxy, gridWidth, gridHeight
         };
     }
 
     Sketch = (p) => {
-        let x = 100;
-        let y = 100;
+        let canvasWidth = 500;
+        let canvasHeight = 500;
+        const gridHeight = canvasHeight / (this.state.gridSize[3] - this.state.gridSize[2]);
+        const gridWidth = canvasWidth/(this.state.gridSize[1]-this.state.gridSize[0]);
+        this.state.gridSize = this.state.gridSize.slice(0,4).concat([gridWidth, gridHeight]);
 
+        const originx = canvasWidth/2 - (this.state.gridSize[1]+this.state.gridSize[0])*this.state.gridSize[4];
+        const originy = canvasHeight/2 - (this.state.gridSize[3]+this.state.gridSize[2])*this.state.gridSize[4];
+
+        console.log(this.state.gridSize);
+        console.log(originx);
         p.setup = () => {
-            p.createCanvas(200,200);
+            p.createCanvas(canvasWidth,canvasHeight);
         }
    
         p.draw = () => {
-            p.background(0);
+            p.background(240);
             p.fill(255);
-            p.rect(x,y,50,50);
+         
+            
+            for(var i = this.state.gridSize[2]; i<=this.state.gridSize[3]; i++){ //vertical
+                for(var j = this.state.gridSize[0]; j<=this.state.gridSize[1]; j++){ //horizontal
+                    p.strokeWeight(1);
+                    p.stroke(200);
+                    p.line(originx+i*gridWidth, 0, originx+i*gridWidth, canvasHeight);
+                    p.line(0,originy+j*gridHeight,canvasWidth,originy+j*gridHeight);
+                }
+            }
+
+            //axis grids
+            p.stroke(0);
+            p.strokeWeight(2);
+            p.line(0, originy, canvasWidth, originy);
+            p.line(originx, 0, originx, canvasHeight);
+            // console.log(originx);
         }
     }
 
